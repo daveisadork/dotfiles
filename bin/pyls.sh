7e269b595945a269a6c45f0d1b9d4d4f6d984561
Dave Hayes <dave@ave81.com>
2018-02-01 07:47:31 -0600

    Better detection of flake8/pyls in virtualenvs

--- bin/pyls.sh
+++ bin/pyls.sh
@@ -2,12 +2,13 @@
 
 if [ -n "$VIRTUAL_ENV" ]
 then
-    PYLS_BIN=`which pyls`
-    if [ $? == 0 ]
+    PYLS_BIN=$(which pyls)
+    if [[ $? == 0 && "$PYLS_BIN" =~ "$VIRTUAL_ENV" ]]
     then
         exec "$PYLS_BIN" "$@"
     fi
 fi
 
-PYTHON_VERSION=`python -V 2>&1 >/dev/null | ( IFS=".$IFS" ; read a b c && echo $b)`
-exec "$HOME/.vim/plugged-nvim/python-support.nvim/autoload/nvim_py$PYTHON_VERSION/bin/pyls" "$@"
+PYTHON_VERSION=$(python -V 2>&1 >/dev/null | ( IFS=".$IFS" ; read a b c && echo $b))
+PYLS_BIN="$HOME/.vim/plugged-nvim/python-support.nvim/autoload/nvim_py$PYTHON_VERSION/bin/pyls"
+exec "$PYLS_BIN" "$@"
