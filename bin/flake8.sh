1d7a897eb7003f22a99b3c063f37fbcae611f98c
Dave Hayes <dave@ave81.com>
2018-03-07 05:51:02 -0600

    Make sure python scripts work with latest homebrew

--- bin/flake8.sh
+++ bin/flake8.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
 
-if [ -n "$VIRTUAL_ENV" ]
-then
-    FLAKE8_BIN=$(which flake8)
-    if [[ $? == 0 && "$FLAKE8_BIN" =~ "$VIRTUAL_ENV" ]]
-    then
-        exec "$FLAKE8_BIN" "$@"
-    fi
-fi
-
-PYTHON_VERSION=$(python -V 2>&1 >/dev/null | ( IFS=".$IFS" ; read a b c && echo $b))
-FLAKE8_BIN="$HOME/.vim/plugged-nvim/python-support.nvim/autoload/nvim_py$PYTHON_VERSION/bin/flake8"
-exec "$FLAKE8_BIN" "$@"
+PYENV=$(dirname "$0")/pyenv.sh
+exec $PYENV flake8 "$@"
--- bin/pyls.sh
+++ bin/pyls.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
 
-if [ -n "$VIRTUAL_ENV" ]
-then
-    PYLS_BIN=$(which pyls)
-    if [[ $? == 0 && "$PYLS_BIN" =~ "$VIRTUAL_ENV" ]]
-    then
-        exec "$PYLS_BIN" "$@"
-    fi
-fi
-
-PYTHON_VERSION=$(python -V 2>&1 >/dev/null | ( IFS=".$IFS" ; read a b c && echo $b))
-PYLS_BIN="$HOME/.vim/plugged-nvim/python-support.nvim/autoload/nvim_py$PYTHON_VERSION/bin/pyls"
-exec "$PYLS_BIN" "$@"
+PYENV=$(dirname "$0")/pyenv.sh
+exec $PYENV pyls "$@"
