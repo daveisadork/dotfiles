a080b4982e57ba2e3674d36bdbc43d8f35c22ca6
Dave Hayes <dwhayes@gmail.com>
2023-06-27 06:51:10 -0500

    Add shell script for term setup

---
+++ term.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+update_terminfo () {
+    local x ncdir terms
+    ncdir="/opt/homebrew/opt/ncurses"
+    terms=(alacritty-direct alacritty tmux tmux-256color)
+
+    mkdir -p ~/.terminfo && cd ~/.terminfo
+
+    if [ -d $ncdir ] ; then
+        # sed : fix color for htop
+        for x in $terms ; do
+            $ncdir/bin/infocmp -x -A $ncdir/share/terminfo $x > ${x}.src &&
+            sed -i '' 's|pairs#0x10000|pairs#32767|' ${x}.src &&
+            /usr/bin/tic -x ${x}.src &&
+            rm -f ${x}.src
+        done
+    else
+        local url
+        url="https://invisible-island.net/datafiles/current/terminfo.src.gz"
+        if curl -sfLO $url ; then
+            gunzip -f terminfo.src.gz &&
+            sed -i '' 's|pairs#0x10000|pairs#32767|' terminfo.src &&
+            /usr/bin/tic -xe ${(j:,:)terms} terminfo.src &&
+            rm -f terminfo.src
+        else
+            echo "unable to download $url"
+        fi
+    fi
+    cd - > /dev/null
+}
+
+update_terminfo
