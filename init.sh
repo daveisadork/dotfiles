195553de53df20deec06db20a0a869a2cec6f9d9
Dave Hayes <dave@servicebell.com>
2024-03-19 10:11:34 -0500

    First pass at an init script

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+#
+set -e
+
+if [[ `uname` == 'Darwin'  ]]; then
+    if (( ! $+commands[brew] )); then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew tap homebrew/command-not-found
+    brew tap homebrew/cask-fonts
+    brew install -q \
+        alacritty \
+        coreutils \
+        font-sauce-code-pro-nerd-font \
+        fzf \
+        git \
+        neovim \
+        iterm2 \
+        pipx \
+        poetry \
+        pyenv \
+        pyenv-virtualenv \
+        reattach-to-user-namespace \
+        ripgrep \
+        tig \
+        tmux \
+        tmux \
+        xz \
+        zsh \
+    ; 
+    ~/.dotfiles/term.sh
+fi
+
+[ ! -f ~/.zshrc ] && ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+[ ! -f ~/.profile ] && ln -s ~/.dotfiles/zsh/profile ~/.profile
+[ ! -f ~/.zprofile ] && ln -s ~/.dotfiles/zsh/profile ~/.zprofile
+[ ! -d ~/.config/nvim ] && ln -s ~/.dotfiles/nvim ~/.config/nvim
+[ ! -d ~/.config/alacritty ] && ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+[ ! -d ~/.tmux ] && ln -s ~/.dotfiles/tmux ~/.tmux
+[ ! -f ~/.tmux.conf ] && ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+[ ! -f ~/.ruff.toml ] && ln -s ~/.dotfiles/ruff.toml ~/.ruff.toml
+[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore
+[ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+
