d1c4ac7083baf7fd08295c1af004daf02756d654
Dave Hayes <dave@servicebell.com>
2025-12-15 08:06:12 -0600

    Un-break rest.nvim

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -7,13 +7,20 @@ return {
       override_file_sorter = true,
       case_mode = "smart_case",
     },
-    fzy_native = {
+    fzf_native = {
+      fuzzy = true,
       override_generic_sorter = false,
       override_file_sorter = false,
+      case_mode = "smart_case",
+    },
+    fzy_native = {
+      override_generic_sorter = true,
+      override_file_sorter = true,
     },
   },
   extensions_list = {
-    "fzf",
+    -- "fzf",
+    -- "fzf_native",
     "fzy_native",
     "terms",
     "themes",
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -53,10 +53,10 @@ return {
         build = "cd deps/fzy-lua-native && make all",
         event = "BufRead",
       },
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      },
+      -- {
+      --   "nvim-telescope/telescope-fzf-native.nvim",
+      --   build = "make",
+      -- },
     },
     opts = function()
       return require "configs.telescope"
@@ -278,14 +278,15 @@ return {
     "vhyrro/luarocks.nvim",
     priority = 1000,
     config = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
   },
   {
     "rest-nvim/rest.nvim",
     dependencies = {
-      "luarocks.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
     },
     keys = {
       {
@@ -313,82 +314,82 @@ return {
         desc = "Rerun the last request",
       },
     },
-    ft = { "http" },
-    config = function()
-      require("rest-nvim").setup {
-        client = "curl",
-        env_file = ".env",
-        env_pattern = "\\.env$",
-        env_edit_command = "tabedit",
-        encode_url = true,
-        skip_ssl_verification = false,
-        custom_dynamic_variables = {},
-        logs = {
-          level = "info",
-          save = true,
-        },
-        keybinds = {},
-        result = {
-          split = {
-            horizontal = false,
-            in_place = false,
-            stay_in_current_window_after_split = true,
-          },
-          behavior = {
-            decode_url = true,
-            show_info = {
-              url = true,
-              headers = true,
-              http_info = true,
-              curl_command = true,
-            },
-            statistics = {
-              enable = true,
-              ---@see https://curl.se/libcurl/c/curl_easy_getinfo.html
-              stats = {
-                { "total_time", title = "Time taken:" },
-                { "size_download_t", title = "Download size:" },
-              },
-            },
-            formatters = {
-              json = "jq",
-              html = function(body)
-                if vim.fn.executable "tidy" == 0 then
-                  return body, { found = false, name = "tidy" }
-                end
-                local fmt_body = vim.fn
-                  .system({
-                    "tidy",
-                    "-i",
-                    "-q",
-                    "--tidy-mark",
-                    "no",
-                    "--show-body-only",
-                    "auto",
-                    "--show-errors",
-                    "0",
-                    "--show-warnings",
-                    "0",
-                    "-",
-                  }, body)
-                  :gsub("\n$", "")
-
-                return fmt_body, { found = true, name = "tidy" }
-              end,
-            },
-          },
-          keybinds = {
-            buffer_local = true,
-            prev = "H",
-            next = "L",
-          },
-        },
-        highlight = {
-          enable = true,
-          timeout = 750,
-        },
-      }
-    end,
+    -- ft = { "http" },
+    -- config = function()
+    --   require("rest-nvim").setup {
+    --     client = "curl",
+    --     env_file = ".env",
+    --     env_pattern = "\\.env$",
+    --     env_edit_command = "tabedit",
+    --     encode_url = true,
+    --     skip_ssl_verification = false,
+    --     custom_dynamic_variables = {},
+    --     logs = {
+    --       level = "info",
+    --       save = true,
+    --     },
+    --     keybinds = {},
+    --     result = {
+    --       split = {
+    --         horizontal = false,
+    --         in_place = false,
+    --         stay_in_current_window_after_split = true,
+    --       },
+    --       behavior = {
+    --         decode_url = true,
+    --         show_info = {
+    --           url = true,
+    --           headers = true,
+    --           http_info = true,
+    --           curl_command = true,
+    --         },
+    --         statistics = {
+    --           enable = true,
+    --           ---@see https://curl.se/libcurl/c/curl_easy_getinfo.html
+    --           stats = {
+    --             { "total_time", title = "Time taken:" },
+    --             { "size_download_t", title = "Download size:" },
+    --           },
+    --         },
+    --         formatters = {
+    --           json = "jq",
+    --           html = function(body)
+    --             if vim.fn.executable "tidy" == 0 then
+    --               return body, { found = false, name = "tidy" }
+    --             end
+    --             local fmt_body = vim.fn
+    --               .system({
+    --                 "tidy",
+    --                 "-i",
+    --                 "-q",
+    --                 "--tidy-mark",
+    --                 "no",
+    --                 "--show-body-only",
+    --                 "auto",
+    --                 "--show-errors",
+    --                 "0",
+    --                 "--show-warnings",
+    --                 "0",
+    --                 "-",
+    --               }, body)
+    --               :gsub("\n$", "")
+    --
+    --             return fmt_body, { found = true, name = "tidy" }
+    --           end,
+    --         },
+    --       },
+    --       keybinds = {
+    --         buffer_local = true,
+    --         prev = "H",
+    --         next = "L",
+    --       },
+    --     },
+    --     highlight = {
+    --       enable = true,
+    --       timeout = 750,
+    --     },
+    --   }
+    -- end,
   },
 
   { "folke/lazydev.nvim", ft = "lua", opts = {
