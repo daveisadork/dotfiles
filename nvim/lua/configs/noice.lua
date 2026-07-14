6a13c520cf67796bb54c121da2030d78ea32d583
Dave Hayes <dwhayes@gmail.com>
2026-07-14 07:21:22 -0500

    disable noice messages

--- nvim/lua/configs/noice.lua
+++ nvim/lua/configs/noice.lua
@@ -25,7 +25,7 @@ return {
   messages = {
     -- NOTE: If you enable messages, then the cmdline is enabled automatically.
     -- This is a current Neovim limitation.
-    enabled = true, -- enables the Noice messages UI
+    enabled = false, -- enables the Noice messages UI
     view = "notify", -- default view for messages
     view_error = "notify", -- view for errors
     view_warn = "notify", -- view for warnings
