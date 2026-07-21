d537ca2a961ef157f7443ed4b48a3f9dbb56c2bf
Dave Hayes <dwhayes@gmail.com>
2026-07-21 06:53:44 -0500

    No more x more files to edit

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,12 @@
 require "nvchad.autocmds"
+
+-- Clear the arglist after startup so :q doesn't complain with
+-- "E173: N more files to edit" when launched with multiple files.
+-- The files still open as buffers (:bn / :bp to cycle).
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() > 1 then
+      vim.cmd "%argdelete"
+    end
+  end,
+})
